f4195654cddf79ff2e5e49ac93654c2ec6226928
Anders Andreasen <58475535+andr1976@users.noreply.github.com>
2025-10-13 17:58:51 +0200

    upped testing

--- tests/test_flash.py
+++ tests/test_flash.py
@@ -1,1 +1,17 @@
 from openthermo.flash.michelsen import get_flash_dry
+
+def test_flash_dry():
+    P = 5.0e6
+    T = 300.0
+    names = ["methane", "ethane", "propane", "n-butane"]
+    molefracs = [0.64, 0.06, 0.28, 0.02]
+    flash = get_flash_dry(
+        names,
+        molefracs,
+        P=P,
+        T=T,
+        "eos",
+        "PR",
+    )
+    print(flash)
+    pass
