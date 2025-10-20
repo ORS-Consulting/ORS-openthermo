4a40203a1712e5222dc7709cde7abd3b893387e2
Anders Andreasen <58475535+andr1976@users.noreply.github.com>
2025-10-21 01:40:54 +0200

    Minor hack for missing sigma

--- openthermo/vessel/blowdown.py
+++ openthermo/vessel/blowdown.py
@@ -14,7 +14,8 @@ from openthermo.flash.michelsen import get_flash_dry
 from openthermo.vessel.fire import sb_fire
 import warnings
 
-warnings.filterwarnings("ignore")
+warnings.filterwarnings("once")
+warnings.filterwarnings("ignore", category=ResourceWarning)
 
 
 def hem_release_rate():
--- tests/test_flash.py
+++ tests/test_flash.py
@@ -260,5 +260,18 @@ def test_print_thermo():
 
 
 if __name__ == "__main__":
-    test_flash_pseudo_COCO()
-    # pass
+    P = 5.0e6
+    T = 300.0
+    names = ["methane", "ethane", "propane", "n-butane"]
+    molefracs = [0.64, 0.06, 0.28, 0.02]
+    flash = get_flash_dry(
+        names,
+        molefracs,
+        P=P,
+        T=T,
+        rho="eos",
+        model="PR",
+    )
+    res = flash.flash(P=P, T=T, zs=molefracs)
+    # test_flash_pseudo_COCO()
+    pass
